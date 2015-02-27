8a767c8074c171c82614320a5e1b95929da54ef6
Matt Mickelson <mickelmr@gmail.com>
2015-02-27 17:43:03 -0600

    commit before I decide to go changing things again

---
+++ helpers/tracker/tracker.go
@@ -0,0 +1,70 @@
+package tracker
+
+import (
+	"errors"
+	"net/http"
+	"net/url"
+)
+
+//these are structs copied from SegmentIO
+type Message struct {
+	Type      string `json:"type,omitempty"`
+	MessageId string `json:"messageId,omitempty"`
+	Timestamp string `json:"timestamp,omitempty"`
+	SentAt    string `json:"sentAt,omitempty"`
+}
+
+type Track struct {
+	Context     map[string]interface{} `json:"context,omitempty"`
+	Properties  map[string]interface{} `json:"properties,omitempty"`
+	AnonymousId string                 `json:"anonymousId,omitempty"`
+	UserId      string                 `json:"userId,omitempty"`
+	Event       string                 `json:"event"`
+	Message
+}
+
+//events...so eventful
+type EventType int
+
+const (
+	EventNotSet EventType = iota
+	APIEvent
+	Transaction
+)
+
+type Event struct {
+	Action     string    `json:"action"`
+	Category   string    `json:"category"`
+	Label      string    `json:"label"`
+	TrackingID string    `json:"-"`
+	Type       EventType `json:"eventType"`
+
+	Context    map[string]interface{} `json:"context,omitempty"`
+	Properties map[string]interface{} `json:"properties,omitempty"`
+}
+
+func (ev *Event) SendToGoogleAnalytics() error {
+	if ev.TrackingID == "" {
+		return errors.New("Missing tracking ID")
+	}
+	vals := make(url.Values, 0)
+	vals.Add("v", "1")             //protocol version - required
+	vals.Add("tid", ev.TrackingID) //tracking id
+	vals.Add("cid", "555")         //client id - required
+	vals.Add("t", "event")         //even type
+	vals.Add("ec", ev.Category)    //event category
+	vals.Add("ea", ev.Action)      //event action
+	vals.Add("el", ev.Label)       //event label
+	vals.Add("ev", "200")          //event value
+
+	//more params/detailed information can be found here:
+	//https://developers.google.com/analytics/devguides/collection/protocol/v1/parameters
+
+	resp, err := http.Get("http://www.google-analytics.com/collect?" + vals.Encode())
+	if err != nil {
+		return err
+	}
+	defer resp.Body.Close()
+
+	return nil
+}
--- index.go
+++ index.go
@@ -1,18 +1,47 @@
 package main
 
 import (
+	"encoding/json"
+	"errors"
 	"log"
 
 	"github.com/curt-labs/GoQueue/helpers/rabbitmq"
+	"github.com/curt-labs/GoQueue/helpers/tracker"
 	"github.com/streadway/amqp"
 )
 
-type ConsumerHandler struct {
-}
+type ConsumerHandler struct{}
 
 func (h *ConsumerHandler) HandleMessage(msg *amqp.Delivery) error {
 	if msg != nil {
 		log.Printf("Got message: %s\n", string(msg.Body))
+
+		var event tracker.Event
+
+		if err := json.Unmarshal(msg.Body, &event); err != nil {
+			msg.Nack(false, true)
+			return err
+		}
+
+		switch event.Type {
+		case tracker.APIEvent:
+			if err := event.SendToGoogleAnalytics(); err != nil {
+				msg.Nack(false, true)
+				return err
+			}
+		case tracker.Transaction:
+			msg.Nack(false, true)
+			return errors.New("Transaction event not implemented!")
+		case tracker.EventNotSet:
+			msg.Nack(false, true)
+			return errors.New("Event type not set")
+		default:
+			msg.Nack(false, true)
+			return errors.New("Unknown event type")
+		}
+
+		//acknowledge!
+		msg.Ack(false)
 	}
 	return nil
 }
