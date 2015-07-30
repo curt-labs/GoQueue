6a1dfc0ea811e1dc258565317de26c79bc28a945
stinkyfingers <john_shenk@hotmail.com>
2015-07-30 11:58:18 -0500

    adds indexing

--- handlers/admin.go
+++ handlers/admin.go
@@ -1,9 +1,12 @@
 package handlers
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"github.com/bitly/go-nsq"
+	"gopkg.in/mgo.v2/bson"
+	"io/ioutil"
 	"net/http"
 	"strings"
 )
@@ -11,13 +14,26 @@ import (
 const (
 	AdminPartIndexUrl     = "http://iapi.curtmfg.com/index/part/"
 	AdminCategoryIndexUrl = "http://iapi.curtmfg.com/index/category/"
+	PartIndexErrorUrl     = "http://iapi.curtmfg.com/index/part/error"
+	PartIndexSuccessUrl   = "http://iapi.curtmfg.com/index/part/success"
 )
 
 type AdminHandler struct {
-	ModificationType string `json:"modification_type"`
-	ChangeType       string `json:"change_type"`
-	Identifier       string `json:"id"`
+	ModificationType string        `json:"modification_type"`
+	ChangeType       string        `json:"change_type"`
+	Identifier       string        `json:"id"`
+	TransitID        bson.ObjectId `json:"_id" bson:"_id"`
+	Status           Status        `json:"status" bson:"status"`
+	Error            string        `json:"error" bson:"error"`
 }
+
+type Status string
+
+const (
+	INTRANSIT Status = "In Transit"
+	SUCCESS   Status = "Success"
+	ERROR     Status = "Error"
+)
 
 func (a *AdminHandler) HandleMessage(message *nsq.Message) error {
 
@@ -28,7 +44,8 @@ func (a *AdminHandler) HandleMessage(message *nsq.Message) error {
 
 	switch strings.ToLower(a.ModificationType) {
 	case "part":
-		err = a.index(AdminPartIndexUrl)
+		partIndexErr := a.index(AdminPartIndexUrl)
+		err = a.updatePartIndexRecords(partIndexErr)
 	case "category":
 		err = a.index(AdminCategoryIndexUrl)
 	}
@@ -36,9 +53,28 @@ func (a *AdminHandler) HandleMessage(message *nsq.Message) error {
 	if err != nil {
 		return err
 	}
-
 	message.Finish()
+	return nil
+}
 
+func (a *AdminHandler) updatePartIndexRecords(partIndexError error) error {
+	url := PartIndexSuccessUrl
+	if partIndexError != nil {
+		a.Error = partIndexError.Error()
+		url = PartIndexErrorUrl
+	}
+	j, err := json.Marshal(&a)
+	if err != nil {
+		return err
+	}
+	res, err := http.Post(fmt.Sprintf("%s", url), "application/json", bytes.NewBuffer(j))
+	if err != nil {
+		return err
+	}
+	defer res.Body.Close()
+	if res.StatusCode != 200 {
+		return fmt.Errorf("index called failed with %d", res.StatusCode)
+	}
 	return nil
 }
 
@@ -51,11 +87,14 @@ func (a *AdminHandler) index(endpoint string) error {
 	if res == nil {
 		return fmt.Errorf("%s", "request failed")
 	}
-
+	body, err := (ioutil.ReadAll(res.Body))
+	if err != nil {
+		return err
+	}
 	defer res.Body.Close()
 
 	if res.StatusCode != 200 {
-		return fmt.Errorf("index called failed with %d", res.StatusCode)
+		return fmt.Errorf("index called failed with code: %d. %v", res.StatusCode, string(body))
 	}
 
 	return nil
